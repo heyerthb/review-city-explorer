5f15e83327add0afa088ff83930bdf86a27f9e0b
Matthew_Heyert <heyerthb@gmail.com>
2019-10-06 08:44:12 -0700

    working geo api hit

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ require ('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 
 const PORT = process.env.PORT;
-
 const app = express();
 
 app.use(cors());
@@ -19,20 +19,33 @@ app.listen(PORT, () => console.log('Listening on PORT', PORT));
 
 
 function handleLocationRequest(request, response){
-  try {
-    const locationData = require('./data/geo.json');
-      const location = new Location(request.query.data, locationData.results[0]);
-      response.send(location);
-  } catch (error){
-    handleError(error, response);
-  }
+    // TODO: create the url for the geocode API
+    // TODO: run a get request with superagent 
+    // TODO: send the response from superagent
+    const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEO_API_KEY}`;
+    // you can throw the url into a browser with your api key to make sure you're getting the data you want
+    return superagent.get(URL)
+    .then(res => {
+        console.log('response from geocode api', res.body);
+        const location = new Location(request.query.data, res.body);
+
+        response.send(location);
+      })
+    .catch(error =>{
+        handleError(error, response);
+      })
 }
 
-function Location(query, geoData){
+function Location(query, rawData){
+  //   this.query = query;
+  //   this.formatted_query = geoData.formatted_address;
+  //   this.latitude = geoData.geometry.location.lat;
+  //   this.longitude = geoData.geometry.location.lng;
+
     this.query = query;
-    this.formatted_query = geoData.formatted_address;
-    this.latitude = geoData.geometry.location.lat;
-    this.longitude = geoData.geometry.location.lng;
+    this.formatted_query = rawData.results[0].formatted_address;
+    this.latitude = rawData.results[0].geometry.location.lat;
+    this.longitude = rawData.results[0].geometry.location.lng;
 }
 
 function handleWeatherRequest(request, response){
